b1cbbdee1b16d811ec04c3ef31dcb9a7079d6ec5
Simon Pfreundschuh <simonpf@student.chalmers.se>
2016-02-24 14:08:40 +0100

    Documentation.

--- src/algebra/matrix.h
+++ src/algebra/matrix.h
@@ -5,6 +5,29 @@
 #include "matrix_inverse.h"
 #include "matrix_sum.h"
 
+/**
+ * \brief Wrapper class for symbolic matrix computations.
+ *
+ * The Matrix class template provides an abstraction layer for delayin matrix
+ * computations on a given base matrix type Base and a corresponding vector type
+ * Vector. The following delayed operations are provided:
+ *
+ * - Addition: operator+()
+ * - Subtraction: operator-()
+ * - Multiplication: operator*()
+ * - Computing the inverse matrix: inv()
+ * - Solving the system for a given vector: solve()
+ * - Transpose: transp(const Matrix&)
+ *
+ * All delayed operations return proxy classes representing the computation,
+ * which contain only reference to the matrices and vectors holding the actual
+ * data. The execution of the computation is triggered when the proxy class
+ * is converted to the given base matrix type or vector. During the computation
+ * the operations are forwarded to the base matrix type.
+ *
+ * \tparam Base The base matrix type.
+ * \tparam Vector The corresponding vector type.
+ */
 template
 <
 typename Base,
@@ -18,13 +41,32 @@ public:
     using VectorBase = Vector;
     using MatrixBase = Matrix;
 
-    // ----------------- //
-    // Base constructors //
-    // ----------------- //
+    // -------------------- //
+    //     Constructors     //
+    // -------------------- //
 
-    /* template <typename ...Args> */
-    /* Matrix(Args&... params) */
-    /*     : Base(std::forward<Args>(params)...) {} */
+    Matrix()
+        : Base() {}
+
+    // Copy constructors.
+    Matrix(const Matrix& B) = default;
+    Matrix(Matrix&& B)      = default;
+
+    // Assignent operators.
+    Matrix& operator=(const Matrix&) = default;
+    Matrix& operator=(Matrix&& B)    = default;
+
+    // Copy from base.
+    Matrix(const Base& B)
+        : Base(B) {}
+
+    Matrix(const Base&& B)
+        : Base(B) {}
+
+    operator const Matrix()
+    {
+        return *this;
+    }
 
     // ----------------- //
     //   Multiplication  //
@@ -41,7 +83,6 @@ public:
         Vector w(this->Base::operator*(v));
         return w;
     }
-
 
     // ----------------- //
     // Addition operator //
@@ -75,32 +116,6 @@ public:
         return Product<T>{*this, B};
     }
 
-    Matrix()
-        : Base() {}
-
-    // Copy constructors.
-    Matrix(const Matrix& B) = default;
-    Matrix(Matrix&& B)      = default;
-
-    // Assignent operators.
-    Matrix& operator=(const Matrix&) = default;
-    Matrix& operator=(Matrix&& B)    = default;
-
-    // Copy from base.
-    Matrix(const Base& B)
-        : Base(B) {}
-
-    Matrix(const Base&& B)
-        : Base(B) {}
-
-    operator const Matrix()
-    {
-        return *this;
-    }
-    /* Sum operator+(const MatrixBase &B) */
-    /* { */
-    /*     return Sum(*this, B); */
-    /* } */
 };
 
 #endif // ALGEBRA_MATRIX
