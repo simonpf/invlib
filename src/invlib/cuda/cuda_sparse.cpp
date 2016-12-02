35344eb2a433f9f30d266fc9211dc6a23a5f740d
Simon Pfreundschuh <simonpf@chalmers.se>
2016-12-02 17:40:49 +0100

    More missing files.

---
+++ src/invlib/cuda/cuda_sparse.cpp
@@ -0,0 +1,165 @@
+#define HANDLE_CUDA_ERROR(x) {handle_cuda_error((x), __FILE__, __LINE__);}
+
+template <typename Real>
+cusparseMatDescr CudaSparse<Real, Representation::CompressedColumns>::
+default_matrix_descriptor = {
+    MatrixType : CUSPARSE_MATRIX_TYPE_GENERAL,
+    FillMode   : CUSPARSE_FILL_MODE_LOWER,
+    DiagType   : CUSPARSE_DIAG_TYPE_NON_UNIT,
+    IndexBase  : CUSPARSE_INDEX_BASE_ZERO};
+
+template<typename Real>
+CudaSparse<Real, Representation::CompressedColumns>::CudaSparse(
+    const SparseData<Real, int, Representation::CompressedColumns> & base,
+    CudaDevice & device_)
+    : m(base.rows()), n(base.cols()), nnz(base.non_zeros()), device(device_)
+{
+    const int * row_indices_host = base.get_row_index_pointer();
+    row_indices = std::shared_ptr<int *>(new (int *), int_deleter);
+    HANDLE_CUDA_ERROR(cudaMalloc(reinterpret_cast<void **>(&*row_indices),
+                                 nnz * sizeof(int)));
+    HANDLE_CUDA_ERROR(cudaMemcpy(reinterpret_cast<void *>(*row_indices),
+                                 reinterpret_cast<const void *>(row_indices_host),
+                                 nnz * sizeof(int),
+                                 cudaMemcpyHostToDevice));
+
+    const int * column_starts_host = base.get_column_start_pointer();
+    column_starts = std::shared_ptr<int *>(new (int *), int_deleter);
+    HANDLE_CUDA_ERROR(cudaMalloc(reinterpret_cast<void **>(&*column_starts),
+                                 (n + 1) * sizeof(int)));
+    HANDLE_CUDA_ERROR(cudaMemcpy(reinterpret_cast<void *>(*column_starts),
+                                 reinterpret_cast<const void *>(column_starts_host),
+                                 (n + 1) * sizeof(int),
+                                 cudaMemcpyHostToDevice));
+
+    elements = std::shared_ptr<Real *>(new (Real *), real_deleter);
+    HANDLE_CUDA_ERROR(cudaMalloc(reinterpret_cast<void **>(&*elements),
+                                 nnz * sizeof(Real)));
+    HANDLE_CUDA_ERROR(cudaMemcpy(
+                          reinterpret_cast<void *>(*elements),
+                          reinterpret_cast<const void *>(base.get_element_pointer()),
+                          nnz * sizeof(Real),
+                          cudaMemcpyHostToDevice));
+}
+
+template<typename Real>
+auto CudaSparse<Real, Representation::CompressedColumns>::multiply(
+    const VectorType & v) const
+    -> VectorType
+{
+    VectorType w{};
+    w.resize(m);
+    cusparse::smv<Real, Representation::CompressedColumns>(
+        device.get_cusparse_handle(),
+        CUSPARSE_OPERATION_NON_TRANSPOSE,
+        m, n, nnz, 1.0,
+        reinterpret_cast<cusparseMatDescr_t>(&default_matrix_descriptor),
+        *elements, *column_starts, *row_indices,
+        v.get_element_pointer(), 0.0,
+        w.get_element_pointer());
+    return w;
+}
+
+template<typename Real>
+auto CudaSparse<Real, Representation::CompressedColumns>::transpose_multiply(
+    const VectorType & v) const
+    -> VectorType
+{
+    cusparseMatDescr_t descr;
+    cusparseCreateMatDescr(& descr);
+
+    VectorType w{};
+    w.resize(n);
+    cusparse::smv<Real, Representation::CompressedColumns>(
+        device.get_cusparse_handle(),
+        CUSPARSE_OPERATION_TRANSPOSE,
+        m, n, nnz, 1.0,
+        reinterpret_cast<cusparseMatDescr_t>(&default_matrix_descriptor),
+        *elements, *column_starts, *row_indices,
+        v.get_element_pointer(), 0.0,
+        w.get_element_pointer());
+    return w;
+}
+
+template <typename Real>
+cusparseMatDescr CudaSparse<Real, Representation::CompressedRows>::
+default_matrix_descriptor = {
+    MatrixType : CUSPARSE_MATRIX_TYPE_GENERAL,
+    FillMode   : CUSPARSE_FILL_MODE_LOWER,
+    DiagType   : CUSPARSE_DIAG_TYPE_NON_UNIT,
+    IndexBase  : CUSPARSE_INDEX_BASE_ZERO};
+
+template<typename Real>
+CudaSparse<Real, Representation::CompressedRows>::CudaSparse(
+    const SparseData<Real, int, Representation::CompressedRows> & base,
+    CudaDevice & device_)
+    : m(base.rows()), n(base.cols()), nnz(base.non_zeros()), device(device_)
+{
+    const int * column_indices_host = base.get_column_index_pointer();
+    column_indices = std::shared_ptr<int *>(new (int *), int_deleter);
+    HANDLE_CUDA_ERROR(cudaMalloc(reinterpret_cast<void **>(&*column_indices),
+                                 nnz * sizeof(int)));
+    HANDLE_CUDA_ERROR(cudaMemcpy(reinterpret_cast<void *>(*column_indices),
+                                 reinterpret_cast<const void *>(column_indices_host),
+                                 nnz * sizeof(int),
+                                 cudaMemcpyHostToDevice));
+
+    const int * row_starts_host = base.get_row_start_pointer();
+    row_starts = std::shared_ptr<int *>(new (int *), int_deleter);
+    HANDLE_CUDA_ERROR(cudaMalloc(reinterpret_cast<void **>(&*row_starts),
+                                 (m + 1) * sizeof(int)));
+    HANDLE_CUDA_ERROR(cudaMemcpy(reinterpret_cast<void *>(*row_starts),
+                                 reinterpret_cast<const void *>(row_starts_host),
+                                 (m + 1) * sizeof(int),
+                                 cudaMemcpyHostToDevice));
+
+    elements = std::shared_ptr<Real *>(new (Real *), real_deleter);
+    HANDLE_CUDA_ERROR(cudaMalloc(reinterpret_cast<void **>(&*elements),
+                                 nnz * sizeof(Real)));
+    HANDLE_CUDA_ERROR(cudaMemcpy(
+                          reinterpret_cast<void *>(*elements),
+                          reinterpret_cast<const void *>(base.get_element_pointer()),
+                          nnz * sizeof(Real),
+                          cudaMemcpyHostToDevice));
+}
+
+template<typename Real>
+auto CudaSparse<Real, Representation::CompressedRows>::multiply(
+    const VectorType & v) const
+    -> VectorType
+{
+    VectorType w{};
+    w.resize(m);
+
+    cusparseMatDescr_t descr;
+    cusparseCreateMatDescr(& descr);
+    cudaDeviceSynchronize();
+
+    cusparse::smv<Real, Representation::CompressedRows>(
+        device.get_cusparse_handle(),
+        CUSPARSE_OPERATION_NON_TRANSPOSE,
+        m, n, nnz, 1.0,
+        reinterpret_cast<cusparseMatDescr_t>(&default_matrix_descriptor),
+        *elements, *row_starts, *column_indices,
+        v.get_element_pointer(), 0.0,
+        w.get_element_pointer());
+    return w;
+}
+
+template<typename Real>
+auto CudaSparse<Real, Representation::CompressedRows>::transpose_multiply(
+    const VectorType & v) const
+    -> VectorType
+{
+    VectorType w{};
+    w.resize(n);
+    cusparse::smv<Real, Representation::CompressedRows>(
+        device.get_cusparse_handle(),
+        CUSPARSE_OPERATION_TRANSPOSE,
+        m, n, nnz, 1.0,
+        reinterpret_cast<const cusparseMatDescr_t>(&default_matrix_descriptor),
+        *elements, *row_starts, *column_indices,
+        v.get_element_pointer(), 0.0,
+        w.get_element_pointer());
+    return w;
+}
---
+++ src/invlib/cuda/cuda_sparse.h
@@ -0,0 +1,137 @@
+/**
+ * \file cuda/cuda_sparse.h
+ *
+ * \brief Contains the cuda sparse class which implements sparse matrix
+ * type for use with the cuSparse library.
+ *
+ */
+#ifndef CUDA_CUDA_SPARSE
+#define CUDA_CUDA_SPARSE
+
+#include "invlib/sparse/sparse_data.h"
+#include "invlib/cuda/cusparse_generic.h"
+#include "invlib/cuda/cuda_vector.h"
+
+namespace invlib
+{
+
+/*! CuSparse Matrix descriptor struct. */
+typedef struct {
+    cusparseMatrixType_t MatrixType;
+    cusparseFillMode_t   FillMode;
+    cusparseDiagType_t   DiagType;
+    cusparseIndexBase_t  IndexBase;
+} cusparseMatDescr;
+
+// ------------------------ //
+// Cuda Sparse Matrix Class //
+// ------------------------ //
+
+/**
+ * \brief Sparse Matrix Class for CUDA Architectures
+ *
+ * Template class for sparse matrices in compressed row and compressed column
+ * formats on CUDA devices.
+ *
+ * Provides multiply(...) and multiply_transpose(...) member functions so that
+ * they can be used as matrix type for the CG solver.
+ *
+ */
+template<typename Real, Representation rep> class CudaSparse;
+
+template<typename Real>
+class CudaSparse<Real, Representation::CompressedColumns>
+{
+public:
+
+    // -------------- //
+    //  Type Aliases  //
+    // -------------- //
+
+    using RealType   = Real;
+    using VectorType = CudaVector<RealType>;
+    using MatrixType = CudaSparse;
+    using ResultType = CudaSparse;
+
+    // ------------------------------- //
+    //  Constructors and Destructors   //
+    // ------------------------------- //
+
+    CudaSparse(const SparseData<Real, int, Representation::CompressedColumns> & base,
+               CudaDevice & = Cuda::default_device);
+    CudaSparse(const CudaSparse & )             = delete;
+    CudaSparse(      CudaSparse &&)             = delete;
+    CudaSparse & operator=(const CudaSparse & ) = delete;
+    CudaSparse & operator=(      CudaSparse &&) = delete;
+
+    // -----------//
+    // Arithmetic //
+    // ---------- //
+
+    VectorType multiply(const VectorType &) const;
+    VectorType transpose_multiply(const VectorType &) const;
+
+private:
+
+    static cusparseMatDescr default_matrix_descriptor;
+
+    size_t m, n, nnz;
+
+    CudaDevice & device;
+
+    std::shared_ptr<int *>  column_starts;
+    std::shared_ptr<int *>  row_indices;
+    std::shared_ptr<Real *> elements;
+
+    CudaDeleter<int *>  int_deleter{};
+    CudaDeleter<Real *> real_deleter{};
+};
+
+template<typename Real>
+class CudaSparse<Real, Representation::CompressedRows>
+{
+public:
+
+    // -------------- //
+    //  Type Aliases  //
+    // -------------- //
+
+    using RealType   = Real;
+    using VectorType = CudaVector<RealType>;
+    using MatrixType = CudaSparse;
+    using ResultType = CudaSparse;
+
+    // ------------------------------- //
+    //  Constructors and Destructors   //
+    // ------------------------------- //
+
+    CudaSparse(const SparseData<Real, int, Representation::CompressedRows> & base,
+               CudaDevice & = Cuda::default_device);
+    CudaSparse(const CudaSparse & )             = delete;
+    CudaSparse(      CudaSparse &&)             = delete;
+    CudaSparse & operator=(const CudaSparse & ) = delete;
+    CudaSparse & operator=(      CudaSparse &&) = delete;
+
+    VectorType multiply(const VectorType &) const;
+    VectorType transpose_multiply(const VectorType &) const;
+
+private:
+
+    static cusparseMatDescr default_matrix_descriptor;
+
+    size_t m, n, nnz;
+
+    CudaDevice & device;
+
+    std::shared_ptr<int *>  column_indices;
+    std::shared_ptr<int *>  row_starts;
+    std::shared_ptr<Real *> elements;
+
+    CudaDeleter<int *>  int_deleter{};
+    CudaDeleter<Real *> real_deleter{};
+};
+
+#include "cuda_sparse.cpp"
+
+}      // namespace invlib
+#endif // CUDA_CUDA_SPARSE
