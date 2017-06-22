ee60b27ad760140cd0a7b9d521b86bc05eed5adf
simonpf <simonpf@chalmers.se>
2017-06-22 18:35:29 +0200

    Added missing invlib.h

---
+++ src/invlib/invlib.h
@@ -0,0 +1,14 @@
+/**
+ * @file invlib.h
+ * @author Simon Pfreundschuh
+ * @date 2017-06-22
+ * @brief Invlib dummy class to distinguis external code.
+ *
+ */
+
+#ifndef INVLIB_H
+#define INVLIB_H
+
+class Invlib {};
+
+#endif // INVLIB_H
